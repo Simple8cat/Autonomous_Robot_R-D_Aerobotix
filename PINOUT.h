60d57ae146e592804d99af8a97e04dba412a6bfa
Aymen Turki Zabik <aymenturkizabik@gmail.com>
2025-11-17 18:27:23 +0100

    Add PINOUT.h for motor and encoder pins
    Define motor and encoder pin mappings.

---
+++ PINOUT.h
@@ -0,0 +1,17 @@
+// defining motor pins
+
+#define Motor1A 16
+#define Motor1B 17
+#define Motor2A 18
+#define Motor2B 19
+#define pwm1 25
+#define pwm2 26
+#define StbyPin 27
+
+// defining encoders pins
+
+#define Encoder1A 35
+#define Encoder1B 34
+#define Encoder2A 32
+#define Encoder2B 13
+
